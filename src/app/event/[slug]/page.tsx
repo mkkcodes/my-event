af279636272c72afbae221880b068907ee3ccad1
Min Khant Kyaw <mkhantkyaw61@gmail.com>
2024-11-19 22:42:12 +0000

    static params turned off

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -21,17 +21,17 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export async function generateStaticParams() {
-  //top 100 most popular events
-  return [
-    {
-      slug: "comedy-extravaganza",
-    },
-    {
-      slug: "dj-practice-session",
-    },
-  ];
-}
+// export async function generateStaticParams() {
+//   //top 100 most popular events
+//   return [
+//     {
+//       slug: "comedy-extravaganza",
+//     },
+//     {
+//       slug: "dj-practice-session",
+//     },
+//   ];
+// }
 
 export default async function Event({ params }: Props) {
   const slug = params.slug;
